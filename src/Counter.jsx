7a502709423c4e1126a9e42a8265caf3ce1c479f
Shesco <shedrachafeignatius@gmail.com>
2025-07-22 10:32:43 +0100

    Updated project 10

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -13,10 +13,11 @@ function Counter() {
     
   return (
     <div>
-      <h1 className='text-3xl font-bold text-center'>Counter</h1>
+      
+      <h1 className='flex flex-col items-center justify-center h-screen'>
+        <h1 className='text-3xl font-bold text-center'>Counter</h1>
       <h1 className='text-2xl font-bold text-center'>Using React Redux</h1>
       <h1 className='text-2xl font-bold text-center'>Count: {count}</h1>
-      <h1 className='flex flex-col items-center justify-center h-screen gap-4'>
         <button onClick={() => dispatch(Increment())}>+</button>
         <span className='text-2xl font-bold'> {count}  </span>
         <button onClick={() => dispatch(Decrement())}>-</button>
